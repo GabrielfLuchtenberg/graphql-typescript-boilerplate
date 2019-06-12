d99f931f2392b2f8dc5865a777b2095c18def6b2
Gabriel <gabriel_luchtenberg@edu.univali.br>
2019-06-11 22:33:33 -0300

    Add: email validation

--- src/modules/register/index.test.ts
+++ src/modules/register/index.test.ts
@@ -14,16 +14,34 @@ describe("Register user", () => {
 
   const mutation = `
     mutation {
-      register(email: "${email}", password: "${password}")
+      register(email: "${email}", password: "${password}"){
+        path
+      }
     }
   `;
   it("Should register a user", async () => {
     const response = await testRequest(port, mutation);
-    expect(response).toEqual({ register: true });
+    expect(response).toEqual({ register: null });
     const users = await User.find({ where: { email } });
     expect(users).toHaveLength(1);
     const user = users[0];
     expect(user.email).toEqual(email);
     expect(user.password).not.toEqual(password);
+  });
+  it("Should not be able to register a user", async () => {
+    await testRequest(port, mutation);
+    const response2 = await testRequest(port, mutation);
+    const expectedError = [
+      {
+        path: "email"
+      }
+    ];
+    expect(response2.register).toMatchObject(expectedError);
+    // expect(response).toEqual({ register: null });
+    // const users = await User.find({ where: { email } });
+    // expect(users).toHaveLength(1);
+    // const user = users[0];
+    // expect(user.email).toEqual(email);
+    // expect(user.password).not.toEqual(password);
   });
 });
