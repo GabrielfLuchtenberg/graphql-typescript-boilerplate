f7e6762d5d9ca0a54f876def1969985e287f8c48
Gabriel <gabriel_luchtenberg@edu.univali.br>
2019-06-11 22:02:23 -0300

    create modules on server

--- src/startServer.ts
+++ src/startServer.ts
@@ -1,18 +1,28 @@
+import { importSchema } from "graphql-import";
 import { GraphQLServer } from "graphql-yoga";
-import { resolvers } from "./resolvers";
+import * as path from "path";
+import * as fs from "fs";
+import { mergeSchemas, makeExecutableSchema } from "graphql-tools";
+import { GraphQLSchema } from "graphql";
 import { createOrmConnection } from "./orm-utils";
 
-export async function startServer() {
-  const server = new GraphQLServer({
-    typeDefs: "src/schema.graphql",
-    resolvers
+export const startServer = async () => {
+  const schemas: GraphQLSchema[] = [];
+  const folders = fs.readdirSync(path.join(__dirname, "./modules"));
+  folders.forEach(folder => {
+    const { resolvers } = require(`./modules/${folder}/resolvers`);
+    const typeDefs = importSchema(
+      path.join(__dirname, `./modules/${folder}/schema.graphql`)
+    );
+    schemas.push(makeExecutableSchema({ resolvers, typeDefs }));
   });
-  const port = process.env.NODE_ENV === "test" ? 0 : 4000;
+
+  const server = new GraphQLServer({ schema: mergeSchemas({ schemas }) });
   await createOrmConnection();
   const app = await server.start({
     port: process.env.NODE_ENV === "test" ? 0 : 4000
   });
-  console.log(`Server is running on localhost:${port}`);
+  console.log("Server is running on localhost:4000");
 
   return app;
-}
+};
--- src/tests/utils.ts
+++ src/tests/utils.ts
@@ -1,10 +1,11 @@
 import { request as gRequest } from "graphql-request";
 import { Variables } from "graphql-request/dist/src/types";
-import { startServer } from "../startServer";
-import { AddressInfo } from "net";
 const host = "http://localhost";
-export const testRequest = async (mutation: string, variables?: Variables) => {
-  const app = await startServer();
-  const { port } = app.address() as AddressInfo;
+
+export const testRequest = async (
+  port: number,
+  mutation: string,
+  variables?: Variables
+) => {
   return gRequest(`${host}:${port}`, mutation, variables);
 };
