7c96be68862960d1abddce3f470a2b3dd87e536a
Gabriel <gabriel.luchtenberg12@gmail.com>
2019-07-14 13:41:53 -0300

    add spark post

--- src/process-variables.ts
+++ src/process-variables.ts
@@ -1,3 +1,4 @@
 const TEST_HOST = process.env.TEST_HOST as string;
-
-export { TEST_HOST };
+const SPARKPOST_API_KEY = process.env.SPARKPOST_API_KEY as string;
+const NODE_ENV = process.env.NODE_ENV;
+export { TEST_HOST, SPARKPOST_API_KEY, NODE_ENV };
